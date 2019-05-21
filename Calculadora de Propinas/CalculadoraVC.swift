8bbaf862837fe78f08cb7eb39328aee7c3cbfafa
Sme Angeles <Esmeralda.angelesm@hotmail.com>
2019-05-21 09:34:46 -0500

    - Se añade logica para calcular de acuerdo al porcentaje seleccionado el monto de la propina y el total de la factura mas la propina.

--- Calculadora de Propinas/CalculadoraVC.swift
+++ Calculadora de Propinas/CalculadoraVC.swift
@@ -13,19 +13,57 @@ class CalculadoraVC: UIViewController {
     @IBOutlet weak var tfMontoFactura: UITextField!
     @IBOutlet weak var lblPropina: UILabel!
     @IBOutlet weak var lblTotal: UILabel!
+    @IBOutlet weak var btn20: UIButton!
+    @IBOutlet weak var btn15: UIButton!
+    @IBOutlet weak var btn10: UIButton!
     
+    var porcentaje: Float = 0
+    var arrButtons: [UIButton] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        arrButtons = [btn20,btn15,btn10]
 
     }
     
+    func showAlert(titulo:String ,mensaje: String ){
+        let alert =  UIAlertController(title: titulo, message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        let ok =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func validaCampos()-> Bool{
+        var valido = true
+        if tfMontoFactura.text!.isEmpty{
+            showAlert(titulo: "Ups", mensaje: "Introduce el monto de la factura.")
+            valido = false
+        }
+        return valido
+    }
+    
     @IBAction func acButtonTapped(_ sender: UIButton) {
+        for btn in arrButtons{
+            (btn.tag == sender.tag) ?  (btn.isSelected = true) : (btn.isSelected = false)
+        }
+        porcentaje = Float(sender.tag) / 100
     }
     
     
     @IBAction func actCalcular(_ sender: Any) {
+        let valido = validaCampos()
+        if valido{
+            let factura = Float(tfMontoFactura.text!)!
+            
+            let propina = factura * porcentaje
+            
+            let total = factura + propina
+            
+            lblPropina.text = "$\(propina)"
+            lblTotal.text = "$\(total)"
+        }
     }
     
 }
