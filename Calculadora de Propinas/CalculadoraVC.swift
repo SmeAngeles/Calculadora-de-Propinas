880c88e7db19ba746229f86b60492086a9eee422
Sme Angeles <Esmeralda.angelesm@hotmail.com>
2019-05-21 09:14:21 -0500

    - Se añade el controlador calculadora, para implementar la lógica de la vista.

---
+++ Calculadora de Propinas/CalculadoraVC.swift
@@ -0,0 +1,31 @@
+//
+//  CalculadoraVC.swift
+//  Calculadora de Propinas
+//
+//  Created by Esmeralda Angeles on 5/21/19.
+//  Copyright © 2019 Sme.Angeles. All rights reserved.
+//
+
+import UIKit
+
+class CalculadoraVC: UIViewController {
+
+    @IBOutlet weak var tfMontoFactura: UITextField!
+    @IBOutlet weak var lblPropina: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func acButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func actCalcular(_ sender: Any) {
+    }
+    
+}
